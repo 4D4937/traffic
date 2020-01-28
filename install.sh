5e8d7fa1bc499544bcf23185b1198e079a6a4e8a
Dr. Z <o_u1000@dragons.hutchcc.edu>
2020-01-28 21:17:46 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt update
+apt-get install vnstat vnstati
+vnstat -u -i eth0
+/etc/init.d/vnstat start
+wget https://raw.githubusercontent.com/4D4937/traffic/master/traffic.py
+"*/30 * * * * root python /root/traffic.py >> /etc/crontab
