928cbbafe71b48c1d455a8683cf9f16622b79ef1
Dr. Z <o_u1000@dragons.hutchcc.edu>
2020-01-28 21:21:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ apt-get install vnstat vnstati
 vnstat -u -i eth0
 /etc/init.d/vnstat start
 wget https://raw.githubusercontent.com/4D4937/traffic/master/traffic.py
-"*/30 * * * * root python /root/traffic.py >> /etc/crontab
+"*/30 * * * * root python /root/traffic.py" >> /etc/crontab
